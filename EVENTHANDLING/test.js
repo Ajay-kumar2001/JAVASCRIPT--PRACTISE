85463f147ff627f3e6215b88c78f79b31c007abb
Ajay <bandari.ajaykumar2001@gmail.com>
2023-04-27 21:34:32 -0400

    addeb...

---
+++ EVENTHANDLING/test.js
@@ -0,0 +1,16 @@
+let fun=()=>{
+    console.log("hello")
+}
+let fun1=()=>{
+    console.log("1 hello")
+}
+let fun2=()=>{
+    console.log(" 2 hello")
+}
+let fun3=()=>{
+    console.log(" 3 hello")
+}
+fun()
+fun1()
+fun2()
+fun3()  
